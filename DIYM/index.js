f59b4173d729511365814169c678f3600a62ed14
mrjones-plip <ashley@medicmobile.org>
2021-06-27 23:00:03 -0700

    WIP for DIYM, functional POST reciever, dev env

--- DIYM/index.js
+++ DIYM/index.js
@@ -1,17 +1,31 @@
 
+console.log('START')
 'use strict'
-
 import express from 'express'
-
 const app = express()
+app.use(express.json());
 
-// Any request regardless of request type or url path to the mediator port will be caught here
-// and trigger the Hello World response.
-app.all('*', (_req, res) => {
-    res.send('Hello World')
+// Allow port override for testing
+let port = 5051
+if(process.env.DEV_PORT){
+    port = process.env.DEV_PORT
+}
+
+// process POSTs to /join_object/:foriegnSystem
+// can test with:
+//   curl -X POST -H "Content-Type: application/json" -d @cht-config/sample.patient.json http://localhost:5053/join_object/chT
+app.post('/join_object/:foriegnSystem', function (req, res) {
+    console.log('GOT POST')
+    const foriegnSystem = req.params.foriegnSystem.toLowerCase()
+    const cht_id = req.body.identifier.value
+    if (foriegnSystem == 'cht' && cht_id != undefined){
+        console.log('   GOT CHT ID: ' , cht_id)
+    }
+    res.send('  Thanks!')
 })
 
-app.listen(5051, () => {
-    console.log('Server listening on port 5051...')
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`)
 })
 
