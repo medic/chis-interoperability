f65f82b732f43c1e69b4d1679b33d486affb2299
mrjones-plip <ashley@medicmobile.org>
2021-06-28 20:10:54 -0700

    WIP for DIYM, fix error handling

--- DIYM/index.js
+++ DIYM/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 // todo - put URL, login, password in a config file - add dist of config file
-const OpenHimURL = 'https://test:tesst@192-168-68-40.my.local-ip.co:5002'
+const OpenHimURL = 'https://test:test@192-168-68-40.my.local-ip.co:5002'
 
 // Allow port override for testing
 let port = 5051
@@ -23,24 +23,46 @@ if(process.env.DEV_PORT){
 //   DEV_PORT=5053 nodemon index.js
 // todo - add authentication to this request
 app.post('/join_object/:foriegnSystem', function (req, res) {
-    let himId
+    let lookupResult
     let sent = false
+    let validId
+    let cht_id
+    let response = {'id':'','result':'error', 'error': ''}
     const foriegnSystem = req.params.foriegnSystem.toLowerCase()
-    const cht_id = req.body.identifier.value
-    if (foriegnSystem == 'cht' && cht_id != undefined){
+
+    try {
+        cht_id = req.body.identifier.value + 1
+        validId = true
+    } catch (e) {
+        console.log('Error while reading POST data', e.message);
+        response.error = 'Cannot read POST data. Error: ' + e.message
+        response.result = 'error'
+
+        let status = 500;
+        res.status(status).send(response)
+        validId = false
+    }
+
+    if (foriegnSystem == 'cht' && validId == true){
         getOpenHimIdByForiegnId(OpenHimURL, 'Patient', cht_id)
-            .then(himId => {
-                if (himId != false) {
+            .then(lookupResult => {
+                if (lookupResult.result == 'found') {
                     let status = 200;
-                    res.status(status).send({'id': himId, 'result': 'found'})
-                } else {
+                    console.log('Found!', lookupResult);
+                    res.status(status).send(lookupResult)
+                } else if (lookupResult.result == 'not_found') {
                     let status = 404;
-                    res.status(status).send({'id': '', 'result': 'not_found'})
+                    console.log('Note Found!', lookupResult);
+                    res.status(status).send(lookupResult)
+                } else {
+                    let status = 500;
+                    console.log('error!', lookupResult);
+                    res.status(status).send(lookupResult)
                 }
             })
             .catch(e => {
                 let status = 500;
-                res.status(status).send({ 'id': '', 'result' : 'error' })
+                res.status(status).send(lookupResult)
             })
     }
     console.log('   DONE')
@@ -51,14 +73,18 @@ app.listen(port, () => {
 })
 
 const getOpenHimIdByForiegnId = function(openHimURL, objectType, foriegnId){
+    console.log('getOpenHimIdByForiegnId start ', openHimURL, objectType, foriegnId);
     let url
-    let result
+    let response = {'id':'','result':'error', 'error': ''}
     const queryPath = '/fhir/' + objectType + '?identifier=' + foriegnId
 
     try {
         url = new URL(queryPath, openHimURL);
     } catch (e) {
         console.log('Error while creating url. Error: ', e.message);
+        response.error = e.message
+        response.result = 'error'
+        return response;
     }
 
     const options = {
@@ -69,13 +95,18 @@ const getOpenHimIdByForiegnId = function(openHimURL, objectType, foriegnId){
     return request.get(options)
         .then(result => {
             if (typeof result.entry == 'object' && result.entry[0].resource.id != undefined){
-                return result.entry[0].resource.id
+                response.id = result.entry[0].resource.id
+                response.result = 'found'
+                return response
+            } else {
+                response.result = 'not_found'
+                return response
             }
-            return false
         })
         .catch(e => {
-            console.log('An error while getting the user - ', e.message);
-            return false
+            console.log('An error while getting the user - ', e.message)
+            response.error = 'getOpenHimIdByForiegnId: ' + e.message
+            response.result = 'error'
+            return response
         })
-
 }
